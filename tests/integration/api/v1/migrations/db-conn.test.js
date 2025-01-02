12a418d0a505e80ea32f326b11fdd931e0408777
Andrey Allyson <andreyuser47@gmail.com>
2025-01-02 14:30:17 -0300

    test: add test for /api/v1/migrations endpoint database connections

---
+++ tests/integration/api/v1/migrations/db-conn.test.js
@@ -0,0 +1,31 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public");
+}
+
+beforeAll(cleanDatabase);
+
+test("Any Method for /api/v1/migrations should close db connection", async () => {
+  const methods = ["GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "PATCH"];
+
+  for (const method of methods) {
+    await fetch("http://localhost:3000/api/v1/migrations", {
+      method,
+    });
+
+    const statusResponse = await fetch("http://localhost:3000/api/v1/status");
+
+    expect(statusResponse.status).toBe(200);
+
+    const statusBody = await statusResponse.json();
+
+    const databaseService = statusBody.services.find(
+      (service) => service.service_name === "database"
+    );
+
+    expect(databaseService).toBeDefined();
+
+    expect(databaseService.database_opened_connections).toBe(1);
+  }
+});
