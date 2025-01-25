1b96161a70fb6feb21196ef89afa13c5cb65af69
Andrey Allyson <andreyuser47@gmail.com>
2025-01-25 15:28:01 -0300

    fix: fix database connection parameters

--- infra/database.js
+++ infra/database.js
@@ -25,7 +25,7 @@ async function getConnectedClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER + "2",
+    user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
