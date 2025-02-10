46371544643a4d7172c3dacbf6d58f91c134d55f
Andrey Allyson <andreyuser47@gmail.com>
2025-02-10 18:06:05 -0300

    refactor: place migrations logic into a model

---
+++ models/migrator.js
@@ -0,0 +1,53 @@
+import database from "infra/database";
+import pgMigrationRunner from "node-pg-migrate";
+import path from "node:path";
+
+const defaultMigratorConfig = {
+  dir: path.resolve("infra", "migrations"),
+  verbose: true,
+  migrationsTable: "pgmigrations",
+  direction: "up",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getConnectedClient();
+
+    const migrationResult = await pgMigrationRunner({
+      ...defaultMigratorConfig,
+      dryRun: true,
+      dbClient,
+    });
+
+    return migrationResult;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getConnectedClient();
+
+    const migrationResult = await pgMigrationRunner({
+      ...defaultMigratorConfig,
+      dryRun: false,
+      dbClient,
+    });
+
+    return migrationResult;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
